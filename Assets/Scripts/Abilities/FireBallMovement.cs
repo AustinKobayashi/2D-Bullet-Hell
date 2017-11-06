4febfa058fc8340e916bc27d896dbd26596ca010
Unknown <ciuffi9@mac.com>
2017-11-05 18:28:49 -0800

    Abstracting
    Abstract player attack and projectile movements.
    Modify enemy bullet movement, fireball movement and player attack movement to override abstract classes

---
+++ Assets/Scripts/Abilities/FireBallMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallMovement : AbstractProjectileMovement {
+
+	private AbilityControls abilityControls;
+
+	public FireBallMovement()
+	{
+		targetTag = "Enemy";
+	}
+
+	public override void hit(Collider2D coll) {
+		abilityControls.CmdDealDamage(coll.gameObject);
+	}
+		
+	public void SetAbilityControls(AbilityControls abilityControls){
+		this.abilityControls = abilityControls;
+	}
+}
