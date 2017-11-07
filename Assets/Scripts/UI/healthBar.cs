57502702ff41a14a5c7e949458361cd9734b4956
Unknown <ciuffi9@mac.com>
2017-11-07 13:37:46 -0800

    Healthbars
    Add player healthbar

---
+++ Assets/Scripts/UI/healthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour
+{
+	private AbstractPlayerStats stats;
+	public GameObject Player;
+	public GameObject Bar;
+	public Image Health;
+	// Use this for initialization
+	void Start ()
+	{
+		stats = Player.GetComponent<AbstractPlayerStats>();
+		Health = Bar.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Health.fillAmount = stats.GetHealth() / 100f;
+		Debug.Log(stats.GetHealth() / 100f);
+	}
+
+}
