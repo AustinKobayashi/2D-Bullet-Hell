c50f23b11c5cae61f082570d0e0e6b6d5c487791
Giulio Rossi <ciuffi9@mac.com>
2017-11-16 20:26:09 -0800

    Fixed minor bug that continue movement with inventory open

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -19,7 +19,10 @@ public class PlayerMovement : NetworkBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!isLocalPlayer) return;
-		if (GetComponent<InventoryHandler>().MenuOpen) return;
+		if (GetComponent<InventoryHandler>().MenuOpen) {
+			rigid.velocity = Vector2.zero;
+			return;
+		}
 
         moveVec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
