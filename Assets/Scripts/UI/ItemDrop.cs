9d9dc3d2aefd0f3e08b84df739cc91eb0a281201
Giulio Rossi <ciuffi9@mac.com>
2017-11-16 20:16:18 -0800

    Fixed Item Drop not properly displaying on new clients

--- Assets/Scripts/UI/ItemDrop.cs
+++ Assets/Scripts/UI/ItemDrop.cs
@@ -2,21 +2,35 @@
 using UnityEngine.Networking;
 
 public class ItemDrop : NetworkBehaviour {
+	[SyncVar]
+	private int _itemId;
 	private Item _item;
-
 	private ItemDatabase _database;
+	private bool _updated;
 	// Use this for initialization
 
-	[ClientRpc]
-	public void RpcSetItem(int i) {
+	public void SetItem(int i) {
+		_itemId = i;
+		UpdateItem();
+	}
+
+	private void UpdateItem() {
 		_database = GameObject.FindGameObjectWithTag("ItemDatabase").GetComponent<ItemDatabase>();
-		_item = _database.GetItem(i);
+		_item = _database.GetItem(_itemId);
 		gameObject.GetComponent<SpriteRenderer>().sprite = _item.GetItemImage();
+		_updated = true;
 	}
 
 	private void OnTriggerEnter2D(Collider2D other) {
-		if (!other.gameObject.CompareTag("Player")) return;
+		if (!isServer || !other.gameObject.CompareTag("Player")) return;
 		other.gameObject.GetComponent<Inventory>().CmdAddItem(_item);
 		Destroy(gameObject);
 	}
+
+	void Update() {
+		if (!_updated) {
+			UpdateItem();
+		}
+	}
+	
 }
